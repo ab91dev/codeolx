156ef193330f798219817cc2e9bb973561b0dd67
ab91dev <ankit.singh.ab91@gmail.com>
2026-09-16 13:12:40 +0530

    add: http server.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("server is running...")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type","application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	srv := http.Server{
+		Addr: ":8000",
+		Handler: mux,
+		ReadTimeout: time.Second * 10,
+		WriteTimeout: time.Second * 40,
+		IdleTimeout: time.Second * 120,
+	}
+
+	err := srv.ListenAndServe()
+	if(err != nil){
+		log.Fatalf("Server failed: %v",err)
+	}
 }
