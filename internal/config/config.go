6852b992e1ae0c8f770310cfd2370bb1fb5ee1a0
ab91dev <ankit.singh.ab91@gmail.com>
2026-09-16 17:15:25 +0530

    add: create configuration loader for server port

---
+++ internal/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string
+}
+
+func MustLoad() Config {
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == ""{
+		panic("PORT is required")
+	}
+
+	return Config{
+		Port: port,
+	}
+}
